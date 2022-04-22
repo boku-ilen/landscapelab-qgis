5f2879eef96c06dfabd8ea75ea8ca565bff6ec52
chrisgraf <christoph.graf@boku.ac.at>
2022-04-22 17:11:40 +0200

    * updated rendering to parallel map rendering to improve performance. Sadly a full map rendering costs approx. 2 seconds

--- RemoteRendering.py
+++ RemoteRendering.py
@@ -4,6 +4,8 @@ from qgis.utils import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import QSize, QBuffer, QByteArray
 
+from datetime import datetime
+
 from .Communicator import Communicator
 
 MESSAGE_CATEGORY = "LL Remote Rendering"
@@ -19,26 +21,20 @@ class RemoteRendering(QgsTask):
     def __init__(self):
         super().__init__('remote control listener task', QgsTask.CanCancel)
         self.communicator = Communicator(self)
-        QgsMessageLog.logMessage('setting up RemoteRendering Task',
-                                 MESSAGE_CATEGORY, Qgis.Info)
+        QgsMessageLog.logMessage('setting up RemoteRendering Task', MESSAGE_CATEGORY, Qgis.Info)
 
     # executes the main task (start listening and waiting for connections)
     def run(self):
         self.active = True
-        QgsMessageLog.logMessage('starting to listen for messages',
-                                 MESSAGE_CATEGORY, Qgis.Info)
+        QgsMessageLog.logMessage('starting to listen for messages', MESSAGE_CATEGORY, Qgis.Info)
         self.communicator.start()
-        QgsMessageLog.logMessage('stop listening for messages',
-                                 MESSAGE_CATEGORY, Qgis.Info)
+        QgsMessageLog.logMessage('stop listening for messages', MESSAGE_CATEGORY, Qgis.Info)
         self.communicator.stop()
         self.active = False
         return True
 
     # reads request and acts accordingly
     def handle_rendering_request(self, request: dict) -> dict:
-
-        QgsMessageLog.logMessage("received message: {}".format(request),
-                                 MESSAGE_CATEGORY, Qgis.Info)
 
         # extract information from request
         target_name = request["target"]
@@ -47,6 +43,7 @@ class RemoteRendering(QgsTask):
 
         extent = QgsRectangle(float(request["extent"]["x_min"]), float(request["extent"]["y_min"]),
                               float(request["extent"]["x_max"]), float(request["extent"]["y_max"]))
+
         if not extent.isFinite():
             QgsMessageLog.logMessage("ERROR: Invalid Extent! Aborting rendering process.",
                                      MESSAGE_CATEGORY, Qgis.Critical)
@@ -66,8 +63,6 @@ class RemoteRendering(QgsTask):
             "extent": request["extent"],
             "image": rendered_image.decode("ascii")
         }
-        QgsMessageLog.logMessage("answer message: {}".format(answer),
-                                 MESSAGE_CATEGORY, Qgis.Info)
         return answer
 
     # cancels the task
@@ -83,41 +78,42 @@ class RemoteRendering(QgsTask):
 
 
 # code mainly from https://github.com/opensourceoptions/pyqgis-tutorials/blob/master/015_render-map-layer.py
-# renders the requested map extent and returns the image as string? TODO
+# renders the requested map extent and returns the image as BASE64 encoded PNG
 def render_image(extent: QgsRectangle, crs: QgsCoordinateReferenceSystem, image_width: int):
-
-    # create image
-    ratio = extent.width() / extent.height()
-    img = QImage(QSize(image_width, image_width // ratio), QImage.Format_ARGB32_Premultiplied)
-
-    # set background color
-    color = QColor(255, 255, 255, 0)
-    img.fill(color.rgba())
 
     # create map settings
     ms = QgsMapSettings()
+
+    color = QColor(255, 255, 255, 0)
     ms.setBackgroundColor(color)
+    ms.setExtent(extent)
+    ms.setDestinationCrs(crs)
+
+    ratio = extent.width() / extent.height()
+    size = QSize(image_width, image_width // ratio)    
+    ms.setOutputSize(size)
 
     # set layers to render
     layers = QgsProject.instance().layerTreeRoot().layerOrder()
     ms.setLayers(layers)
     # TODO: define layers via parameters
 
-    # set extent & crs
-    ms.setExtent(extent)
-    ms.setDestinationCrs(crs)
-    ms.setOutputSize(img.size())
-
     # render image
-    qp = QPainter(img)
-    render = QgsMapRendererCustomPainterJob(ms, qp)
+    render = QgsMapRendererParallelJob(ms)
+    QgsMessageLog.logMessage('pre-render {}'.format(datetime.now()), MESSAGE_CATEGORY, Qgis.Info)
     render.start()
     render.waitForFinished()
-    qp.end()
+    QgsMessageLog.logMessage('post-render {} ({})'.format(datetime.now(), render.renderingTime()), MESSAGE_CATEGORY, Qgis.Info)
+
+    # store result in ByteArray
+    img = render.renderedImage()
     ba = QByteArray()
     buf = QBuffer(ba)
     buf.open(QBuffer.WriteOnly)
     img.save(buf, 'PNG')
+    # QgsMessageLog.logMessage('post-png {}'.format(datetime.now()), MESSAGE_CATEGORY, Qgis.Info)
 
     # encode as Base64 for network transport
-    return ba.toBase64(QByteArray.Base64Encoding).data()
+    # TODO: we could try to change transfer method to BSON or MsgPack and directly transfer binary data
+    base64 = ba.toBase64(QByteArray.Base64Encoding).data()
+    return base64
